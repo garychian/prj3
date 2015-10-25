46458b74fbcb0f5571e42604bbf50dde9ed73dd4
Stewart Boyd <stewartboyd1988@gmail.com>
2015-10-25 14:25:14 -0400

    Udacity accepted part1

--- handle_with_curl.c
+++ handle_with_curl.c
@@ -16,30 +16,44 @@ struct MemoryStruct{
 };
 
 size_t write_memory_cb(void *contents, size_t size, size_t nmemb, void *userp);
+int send_contents(gfcontext_t *ctx, struct MemoryStruct * data);
 void mem_struct_init(struct MemoryStruct *mem);
 
 ssize_t handle_with_curl(gfcontext_t *ctx, char *path, void* arg){
 
-	size_t file_len, bytes_transferred;
-	ssize_t read_len, write_len, remaining_bytes;
 	char buffer[4096];
 	char *data_dir = arg;
 	struct MemoryStruct data;
 	CURLcode curl_ret_code;
+
 	mem_struct_init(&data);
 	strcpy(buffer,data_dir);
 	strcat(buffer,path);
+
 	CURL *easy_handle = curl_easy_init();
+	//fprintf(stdout, "cur_easy_perform.data_dir = %s\n", data_dir);
+	//fprintf(stdout, "cur_easy_perform.path = %s\n", path);
+	fprintf(stdout, "cur_easy_perform.path = %s\n", buffer);
 	curl_easy_setopt(easy_handle, CURLOPT_URL, buffer);
+	curl_easy_setopt(easy_handle, CURLOPT_FAILONERROR, 1);
 	curl_easy_setopt(easy_handle, CURLOPT_WRITEFUNCTION, write_memory_cb);
 	curl_easy_setopt(easy_handle, CURLOPT_WRITEDATA, (void *)&data);
 	curl_ret_code = curl_easy_perform(easy_handle);
 	//performed okay
 	if (curl_ret_code == 0){
+		printf("%lu bytes retrieved\n", (long)data.size);
+		gfs_sendheader(ctx, GF_OK, data.size);
+		/* Sending the file contents chunk by chunk. */
+		int ret_sc = send_contents(ctx, &data);
+		if (ret_sc == 0)
+			return data.size;
+		else
+			return EXIT_FAILURE;
+
 
 	}
 	//couldn't resolve host
-	else if (curl_ret_code == 6){
+	else if (curl_ret_code == 22){
 		return gfs_sendheader(ctx, GF_FILE_NOT_FOUND, 0);
 	}
 	//any other error, it must have been server error. gfserver library will handle
@@ -47,25 +61,36 @@ ssize_t handle_with_curl(gfcontext_t *ctx, char *path, void* arg){
 		return EXIT_FAILURE;
 	}
 
+}
+int send_contents(gfcontext_t *ctx, struct MemoryStruct * data)
+/*
+*arguments*
+gfcontext_t
+MemoryStruct -> struct containing data to send and its size
 
-	printf("%lu bytes retrieved\n", (long)data.size);
-	gfs_sendheader(ctx, GF_OK, data.size);
+*synopsis*
+Sends chunks of data-> memory until bytes_transferred is less than
+the size referenced by data struct.
 
-	/* Sending the file contents chunk by chunk. */
-	bytes_transferred = 0;
-	while(bytes_transferred < data.size){
-		remaining_bytes = data.size - bytes_transferred;
-		read_len = (BUFFER_LEN > remaining_bytes) ? remaining_bytes : BUFFER_LEN;
-		write_len = gfs_send(ctx, data.memory + bytes_transferred, read_len);
-		if (write_len != read_len){
+*return*
+0--If no error
+1--if a gfs_send sends lses than the write_len_blk
+*/
+{
+	ssize_t bytes_transferred = 0;
+	ssize_t remaining_bytes, write_len_blk, write_len;
+	while(bytes_transferred < data->size)
+	{
+		remaining_bytes = data->size - bytes_transferred;
+		write_len_blk = (BUFFER_LEN > remaining_bytes) ? remaining_bytes : BUFFER_LEN;
+		write_len = gfs_send(ctx, data->memory + bytes_transferred, write_len_blk);
+		if (write_len != write_len_blk){
 			fprintf(stderr, "handle_with_file write error");
 			return EXIT_FAILURE;
 		}
 		bytes_transferred += write_len;
 	}
-	curl_easy_cleanup(easy_handle);
-	free(data.memory);
-	return bytes_transferred;
+	return 0;
 }
 
 // reference for http://curl.haxx.se/libcurl/c/getinmemory.html
